1d27726668a90145e793789d6fbd7a0d6ef054f8
PradeepKumar <PradeepKumarRebbavarapu705@gmail.com>
2022-07-27 12:03:53 +0530

    i

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,3 +140,6 @@ INTERNAL_IPS = [
 
 
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"

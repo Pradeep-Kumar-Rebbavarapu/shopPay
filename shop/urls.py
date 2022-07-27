98f47a2cf03b428459b428b05f524c8b3a64ea9f
PradeepKumar <PradeepKumarRebbavarapu705@gmail.com>
2022-07-27 13:56:29 +0530

    i

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,8 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.home,name='HomePage'),
+    path('/login',views.login,name="LoginPage"),
+    path('/Signup',views.signup,name="SignupPage"),
+    path('/Shop',views.shop,name="ShopPage"),
+    path('/Aboutus',views.aboutus,name="AboutusPage"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,5 +4,19 @@ from django.shortcuts import render
 
 def home(request):
     print('entered into home')
-    
     return render(request,'Pages/HomePage.html')
+
+def signup(request):
+    return render(request,'Pages/SignupPage.html')
+
+def loginup(request):
+    return render(request,'Pages/LoginPage.html')
+
+def shop(request):
+    return render(request,'Pages/ShopPage.html')
+
+def contactus(request):
+    return render(request,'Pages/ContactusPage.html')
+    
+def aboutus(request):
+    return render(request,'Pages/AboutusPage.html')
